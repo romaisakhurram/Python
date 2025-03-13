c820917d1bb7b794b9304e67a0514ccacaf346a3
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-03-13 21:02:55 +0500

    update

--- file_convertor/file-convertor.py
+++ file_convertor/file-convertor.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 from io import BytesIO
+import openpyxl
 
 st.set_page_config(page_title="File Convertor", layout="wide")
 st.title("File Convertor & Cleaner")
